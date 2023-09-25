b08ca8e7f90c12082454dc8b70fe1a20afad796c
Christophe Marchand <cmarchand@clever-age.com>
2023-09-25 20:43:40 +0200

    Now defines project properties for release-maven-plugin

--- src/main/java/top/marchand/maven/plugins/VersionCalculatorMavenPlugin.java
+++ src/main/java/top/marchand/maven/plugins/VersionCalculatorMavenPlugin.java
@@ -20,6 +20,7 @@ import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
 
 import java.util.Arrays;
 import java.util.function.Function;
@@ -27,6 +28,9 @@ import java.util.function.Function;
 @Mojo(name = "calculate")
 public class VersionCalculatorMavenPlugin extends AbstractMojo {
 
+  private static final String MAVEN_RELEASE_PLUGIN_RELEASE_VERSION = "releaseVersion";
+  private static final String MAVEN_RELEASE_PLUGIN_DEVELOPMENT_VERSION = "developmentVersion";
+  public static final String NONE = "NONE";
   @Parameter(defaultValue = "${project.version}")
   private String currentVersion;
 
@@ -34,27 +38,57 @@ public class VersionCalculatorMavenPlugin extends AbstractMojo {
    * The type of release to calculate.
    * Legal values are PATCH, MINOR, MAJOR
    */
-  @Parameter(defaultValue = "PATCH")
+  @Parameter(defaultValue = NONE)
   private String releaseType;
+
+  @Parameter(defaultValue = "${project}")
+  private MavenProject project;
 
   @Override
   public void execute() throws MojoExecutionException, MojoFailureException {
+    if(NONE.equals(releaseType)) return;
     try {
       VersionCalculator versionCalculator = new VersionCalculator(currentVersion);
       ReleaseTypeStrategy wrapper = ReleaseTypeStrategy.of(releaseType);
-      String release = wrapper.nextRelease(versionCalculator);
-      String snapshot = wrapper.nextSnapshot(versionCalculator);
-      String result = "{\"release\":\""+ release +"\",\"nextSnapshot\":\""+ snapshot +"\"}";
-      System.out.println(result);
-      getLog().info("release: "+release);
-      getLog().info("snapshot: "+snapshot);
+      NextRelease next = wrapper.calculateNextRelease(versionCalculator);
+      logJson(next);
+      logResults(next);
+      setMavenReleasePluginProperties(next);
     } catch(VersionCalculatorException ex) {
-      getLog().error("The version you try to release do not match SemVer specification : "+currentVersion);
-      getLog().error("See https://semver.org/ and adapt your version");
+      logErrorMessage();
       throw new MojoExecutionException(ex);
     }
   }
 
+  private void logErrorMessage() {
+    getLog().error("The version you try to release does not match SemVer specification : "+currentVersion);
+    getLog().error("See https://semver.org/ and adapt your version");
+  }
+
+  private void logResults(NextRelease next) {
+    getLog().info("release: "+ next.release);
+    getLog().info("snapshot: "+ next.snapshot);
+  }
+
+  private void logJson(NextRelease next) {
+    String result = "{\"release\":\""+ next.release +"\",\"nextSnapshot\":\""+ next.snapshot +"\"}";
+    System.out.println(result);
+  }
+
+  private void setMavenReleasePluginProperties(NextRelease next) {
+    project.getProperties().setProperty(MAVEN_RELEASE_PLUGIN_RELEASE_VERSION, next.release);
+    project.getProperties().setProperty(MAVEN_RELEASE_PLUGIN_DEVELOPMENT_VERSION, next.snapshot);
+  }
+
+  private static class NextRelease {
+    private String release;
+    private String snapshot;
+
+    private NextRelease(String release, String snapshot) {
+      this.release = release;
+      this.snapshot = snapshot;
+    }
+  }
   private enum ReleaseTypeStrategy {
     PATCH(
         "PATCH",
@@ -89,6 +123,12 @@ public class VersionCalculatorMavenPlugin extends AbstractMojo {
           .orElseThrow();
     }
 
+    NextRelease calculateNextRelease(VersionCalculator versionCalculator) {
+      return new NextRelease(
+          nextRelease(versionCalculator),
+          nextSnapshot(versionCalculator)
+      );
+    }
     private String code() {
       return this.code;
     }
